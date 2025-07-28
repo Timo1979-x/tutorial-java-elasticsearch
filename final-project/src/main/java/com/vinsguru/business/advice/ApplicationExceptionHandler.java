2537052b6755a31e87025e4e6ba9a8a72fb93453
Tim Lukashevich <silvernet@yandex.ru>
2025-07-28 21:56:55 +0300

    lesson12-159 Валидация, обработка ошибок

---
+++ final-project/src/main/java/com/vinsguru/business/advice/ApplicationExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.vinsguru.business.advice;
+
+import com.vinsguru.business.exceptions.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+    @ExceptionHandler(value = BadRequestException.class)
+    public ProblemDetail handleException(BadRequestException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+}
--- final-project/src/main/java/com/vinsguru/business/dto/SuggestionRequestParameters.java
+++ final-project/src/main/java/com/vinsguru/business/dto/SuggestionRequestParameters.java
@@ -1,4 +1,16 @@
 package com.vinsguru.business.dto;
 
+import com.vinsguru.business.exceptions.BadRequestException;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
 public record SuggestionRequestParameters(String prefix, Integer limit) {
+    public SuggestionRequestParameters {
+        if (!StringUtils.hasText(prefix)) {
+            throw new BadRequestException("prefix cannot be empty");
+        }
+        limit = Objects.requireNonNullElse(limit, 10);
+    }
+
 }
